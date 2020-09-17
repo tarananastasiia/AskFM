a8712c62d09e92ee7f114f29e53839c12b437a28
vhaliaha <vhaliaha@deloitte.com>
2020-09-17 13:59:37 +0300

    httpdelete(-)

--- AskFM/Controllers/LikesController.cs
+++ AskFM/Controllers/LikesController.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Security.Claims;
+using System.Security.Cryptography.X509Certificates;
 using System.Threading.Tasks;
 using AskFM.Models;
 using Dal.Models;
@@ -21,11 +22,24 @@ namespace AskFM.Controllers
         [HttpPost]
         public IActionResult Add(int questionId, string whoWasLikedId)
         {
+           
             string userId = User.FindFirstValue(ClaimTypes.NameIdentifier);
             var user = _context.Users.Find(userId);
-            user.Likes.Add(new Like {UserId = user.Id,QuestionId=questionId});
+            user.Likes.Add(new Like { UserId = user.Id, QuestionId = questionId });
             _context.SaveChanges();
             return LocalRedirect($"~/question/page?userId={whoWasLikedId}");
+        }
+
+        [HttpDelete]
+        public IActionResult Delete(int questionId, string whoWasLikedId)
+        { 
+            var like = _context.Likes.FirstOrDefault(sc => sc.QuestionId == questionId && sc.UserId == whoWasLikedId);
+            _context.Likes.Remove(like);
+            _context.SaveChanges();
+
+            return new Json { Data = "Deleted", JsonRequestBehavior = JsonRequestBehavior.AllowGet };
+
+            //return LocalRedirect($"~/question/page?userId={whoWasLikedId}");
         }
     }
 }
